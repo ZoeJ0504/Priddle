9a8ee6246c0d4c310e193c32c2c6ecdf2133155e
Grant Ours <grant.ours55@gmail.com>
2022-10-06 11:31:03 -0600

    login/signup working/logout button

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
   resources :groups
-<<<<<<< HEAD
-  resources :posts
+  resources :posts, only: [:index, :update, :create, :destroy]
   resources :users, only: [:show, :create]
   resources :sessions, only: [:create, :destroy]
-=======
-  resources :posts, only: [:index, :update, :create, :destroy]
-  resources :users
->>>>>>> f012290e2914784e19c799b5493a5543e9160b20
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
