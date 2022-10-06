ba03da59c9f72f517de44501b57301048895328a
Grant Ours <grant.ours55@gmail.com>
2022-10-05 19:26:50 -0600

    login changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  post "/login", to: "sessions#create"
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
+  delete "/logout", to: "sessions#destroy"
 end
